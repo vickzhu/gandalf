7ea1549d7609f597d29924f6f3323aff2c8b0d1a
zhuxb <zhuxb@DESKTOP-5GH157O>
2017-08-08 15:05:32 +0800

    算法bug fixed

--- src/main/java/com/gandalf/framework/util/CalculateUtil.java
+++ src/main/java/com/gandalf/framework/util/CalculateUtil.java
@@ -34,7 +34,7 @@ public class CalculateUtil {
     public static double add(double value1, double value2, int scale) {
         BigDecimal b1 = new BigDecimal(Double.toString(value1));
         BigDecimal b2 = new BigDecimal(Double.toString(value2));
-        return b1.add(b2).setScale(scale, RoundingMode.HALF_EVEN).doubleValue();
+        return b1.add(b2).setScale(scale, RoundingMode.HALF_UP).doubleValue();
     }
 
     /**
@@ -59,7 +59,7 @@ public class CalculateUtil {
     public static double sub(double value1, double value2, int scale) {
         BigDecimal b1 = new BigDecimal(Double.toString(value1));
         BigDecimal b2 = new BigDecimal(Double.toString(value2));
-        return b1.subtract(b2).setScale(scale, RoundingMode.HALF_EVEN).doubleValue();
+        return b1.subtract(b2).setScale(scale, RoundingMode.HALF_UP).doubleValue();
     }
 
     /**
@@ -84,7 +84,7 @@ public class CalculateUtil {
     public static double mul(double value1, double value2, int scale) {
         BigDecimal b1 = new BigDecimal(Double.valueOf(value1));
         BigDecimal b2 = new BigDecimal(Double.valueOf(value2));
-        return b1.multiply(b2).setScale(scale, RoundingMode.HALF_EVEN).doubleValue();
+        return b1.multiply(b2).setScale(scale, RoundingMode.HALF_UP).doubleValue();
     }
 
     /**
@@ -111,6 +111,6 @@ public class CalculateUtil {
     public static double div(double value1, double value2, int scale) {
         BigDecimal b1 = new BigDecimal(Double.toString(value1));
         BigDecimal b2 = new BigDecimal(Double.toString(value2));
-        return b1.divide(b2, scale, RoundingMode.HALF_EVEN).doubleValue();
+        return b1.divide(b2, scale, RoundingMode.HALF_UP).doubleValue();
     }
 }
