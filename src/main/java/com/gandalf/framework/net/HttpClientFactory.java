f884b0e2ec30f36a301c5bd3a7fdd00702432c4e
vickzhu <vickzhu@foxmail.com>
2021-03-08 18:14:21 +0800

    upgrade

--- src/main/java/com/gandalf/framework/net/HttpClientFactory.java
+++ src/main/java/com/gandalf/framework/net/HttpClientFactory.java
@@ -20,8 +20,8 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
-import org.apache.http.protocol.ExecutionContext;
 import org.apache.http.protocol.HttpContext;
+import org.apache.http.protocol.HttpCoreContext;
 
 /**
  * 类HttpClientFactory.java的实现描述：HttpClient工厂
@@ -63,7 +63,7 @@ public class HttpClientFactory {
 				// 不要重试SSL握手异常
 				return false;
 			}
-			HttpRequest request = (HttpRequest) context.getAttribute(ExecutionContext.HTTP_REQUEST);
+			HttpRequest request = (HttpRequest) context.getAttribute(HttpCoreContext.HTTP_REQUEST);
 			boolean idempotent = !(request instanceof HttpEntityEnclosingRequest);
 			if (idempotent) {
 				// 如果请求被认为是幂等的，那么就重试
@@ -127,85 +127,4 @@ public class HttpClientFactory {
 			httpClient = httpClientBuilder.build();
 		}
 	}
-
-	// private static class HttpClientHolder {
-	//
-	// /**
-	// * 连接超时时间:实例化链接超时时间
-	// */
-	// private static final int CONNECTION_TIMEOUT = 30 * 1000;
-	//
-	// /**
-	// * 套接字超时时间:获取response的返回超时时间
-	// */
-	// private static final int SOCKET_TIMEOUT = 30 * 1000;
-	//
-	// /**
-	// * 连接池里的最大连接数
-	// */
-	// private static final int MAX_TOTAL_CONNECTIONS = 20;
-	//
-	// /**
-	// * 每个路由的默认最大连接数
-	// */
-	// private static final int MAX_ROUTE_CONNECTIONS = 50;
-	//
-	// /**
-	// * 连接池中 连接请求执行被阻塞的超时时间
-	// */
-	// private static final long CONN_MANAGER_TIMEOUT = 60000;
-	//
-	// private static PoolingClientConnectionManager pcm;
-	//
-	// private static HttpParams httpParams;
-	//
-	// private static DefaultHttpClient httpClient;
-	//
-	// static {
-	// SchemeRegistry schemeRegistry = new SchemeRegistry();
-	// schemeRegistry.register(new Scheme("http", 80,
-	// PlainSocketFactory.getSocketFactory()));
-	// try {
-	// SSLContext ctx = SSLContext.getInstance("TLS");
-	// X509TrustManager tm = new X509TrustManager() {
-	// public X509Certificate[] getAcceptedIssuers() {
-	// return null;
-	// }
-	// public void checkClientTrusted(X509Certificate[] arg0, String arg1)
-	// throws CertificateException {}
-	// public void checkServerTrusted(X509Certificate[] arg0, String arg1)
-	// throws CertificateException {}
-	// };
-	// ctx.init(null, new TrustManager[] { tm }, null);
-	// SSLSocketFactory ssf = new SSLSocketFactory(ctx,
-	// SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
-	// schemeRegistry.register(new Scheme("https", 443, ssf));
-	// } catch (Exception e) {
-	// logger.error("Error when init SSLContext!", e);
-	// }
-	//
-	//// schemeRegistry.register(new Scheme("https", 443,
-	// SSLSocketFactory.getSocketFactory()));
-	// pcm = new PoolingClientConnectionManager(schemeRegistry);
-	// pcm.setDefaultMaxPerRoute(MAX_ROUTE_CONNECTIONS);
-	// pcm.setMaxTotal(MAX_TOTAL_CONNECTIONS);
-	// // pcm.setMaxPerRoute(new HttpRoute(DEFAULT_TARGETHOST), 20); //
-	// 设置对目标主机的最大连接数
-	//
-	// httpParams = new BasicHttpParams();
-	// HttpProtocolParams.setVersion(httpParams, HttpVersion.HTTP_1_1);
-	// HttpConnectionParams.setConnectionTimeout(httpParams,
-	// CONNECTION_TIMEOUT);
-	// HttpConnectionParams.setSoTimeout(httpParams, SOCKET_TIMEOUT);
-	// HttpClientParams.setConnectionManagerTimeout(httpParams,
-	// CONN_MANAGER_TIMEOUT);
-	// // httpParams.setParameter(ClientPNames.CONN_MANAGER_TIMEOUT,
-	// CONN_MANAGER_TIMEOUT);
-	//
-	// httpClient = new DefaultHttpClient(pcm, httpParams);
-	// httpClient.setHttpRequestRetryHandler(new RetryHandler());
-	//
-	// }
-	// }
-
 }
--- src/main/java/com/gandalf/framework/net/HttpTool.java
+++ src/main/java/com/gandalf/framework/net/HttpTool.java
@@ -21,7 +21,6 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.mime.MultipartEntity;
 import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.entity.mime.content.StringBody;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicHeader;
 import org.apache.http.message.BasicNameValuePair;
 import org.slf4j.Logger;
