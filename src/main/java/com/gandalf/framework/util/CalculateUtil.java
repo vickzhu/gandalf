8891a1dd918f7e1d70955e59b57187976538c546
Administrator <Administrator@WINDOWS-AI8T592>
2024-02-29 17:44:46 +0800

    加减乘除精度问题

--- src/main/java/com/gandalf/framework/util/CalculateUtil.java
+++ src/main/java/com/gandalf/framework/util/CalculateUtil.java
@@ -82,8 +82,8 @@ public class CalculateUtil {
      * @return 两个参数的积
      */
     public static double mul(double value1, double value2, int scale) {
-        BigDecimal b1 = new BigDecimal(Double.valueOf(value1));
-        BigDecimal b2 = new BigDecimal(Double.valueOf(value2));
+        BigDecimal b1 = new BigDecimal(Double.toString(value1));
+        BigDecimal b2 = new BigDecimal(Double.toString(value2));
         return b1.multiply(b2).setScale(scale, RoundingMode.HALF_UP).doubleValue();
     }
 
